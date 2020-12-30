9558c97ee6af2d1e2e8c1baeac597b7a763a26a4
Guillaume Trollé <guillaume.trolle@gmail.com>
2020-12-30 19:35:38 +0100

    [build] add missing visual studio files, now the project is dependent on visual studio only. Submodules must be updated and recompiled manually

---
+++ build/extern/glm/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/guill/Source/Repos/kairos_engine/extern/glm
+# Build directory: C:/Users/guill/Source/Repos/kairos_engine/build/extern/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
---
+++ build/extern/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/guill/Source/Repos/kairos_engine/extern/glm/glm
+# Build directory: C:/Users/guill/Source/Repos/kairos_engine/build/extern/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
